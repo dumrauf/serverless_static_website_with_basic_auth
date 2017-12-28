9f83d99be2a550c034ee8216ab851a50948a4769
Dominic Dumrauf (MacBook Pro 2017) <dominic-git-macbook-pro-2017@dumrauf.de>
2017-12-28 21:51:14 +0000

    Handling funky AWS CLI 255 exit codes

--- scripts/bootstrap_serverless_repo.sh
+++ scripts/bootstrap_serverless_repo.sh
@@ -12,16 +12,35 @@ source _configuration.sh
 PROFILE=$1
 
 
+# Makeshift TRY/CATCH block
 {
-    aws cloudformation create-stack --stack-name ${SERVERLESS_CODE_REPOSITORY_STACK_NAME} \
-                                    --template-body file://"${BOOTSTRAP_SERVERLESS_CODE_REPOSITORY_TEMPLATE}" \
-                                    --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" \
-                                    --region ${REGION} \
-                                    --profile ${PROFILE}
+    STDERR=$(aws cloudformation create-stack --stack-name ${SERVERLESS_CODE_REPOSITORY_STACK_NAME} \
+                                             --template-body file://"${BOOTSTRAP_SERVERLESS_CODE_REPOSITORY_TEMPLATE}" \
+                                             --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" \
+                                             --region ${REGION} \
+                                             --profile ${PROFILE} 2>&1)
+
+    # Inspired by <https://gist.github.com/jghaines/6b25a48c8531c236b0ec4831f4465ce4>
+    ERROR_CODE=$?
+    if [[ "${ERROR_CODE}" -eq "255" && "${STDERR}" =~ "AlreadyExistsException" ]]
+    then
+        false
+    else
+        true
+    fi
+
 } || {
-    aws cloudformation update-stack --stack-name ${SERVERLESS_CODE_REPOSITORY_STACK_NAME} \
-                                    --template-body file://"${BOOTSTRAP_SERVERLESS_CODE_REPOSITORY_TEMPLATE}" \
-                                    --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" \
-                                    --region ${REGION} \
-                                    --profile ${PROFILE}
+    STDERR=$(aws cloudformation update-stack --stack-name ${SERVERLESS_CODE_REPOSITORY_STACK_NAME} \
+                                             --template-body file://"${BOOTSTRAP_SERVERLESS_CODE_REPOSITORY_TEMPLATE}" \
+                                             --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" \
+                                             --region ${REGION} \
+                                             --profile ${PROFILE} 2>&1)
+    # Inspired by <https://gist.github.com/jghaines/6b25a48c8531c236b0ec4831f4465ce4>
+    ERROR_CODE=$?
+    if [[ "${ERROR_CODE}" -eq "255" && "${STDERR}" =~ "No updates are to be performed" ]]
+    then
+        exit 0
+    else
+        exit ${ERROR_CODE}
+    fi
 }
